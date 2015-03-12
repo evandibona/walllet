4e7313091ef12325def57b479dbfbd96c52c8f60
Evan DiBona <evandibona@gmx.com>
2015-03-12 11:49:59 -0400

    Controller set up along with a simple service.

---
+++ wallet/App/account.ctrl.js
@@ -0,0 +1,12 @@
+﻿(function() {
+    angular
+        .module("Wallet")
+        .controller("AccManage", accountMan) 
+
+    accountMan.$inject = ['Account'] 
+
+    function accountMan(Account) {
+        var vm = this
+        vm.users = Account.users() 
+    }
+})()
---
+++ wallet/App/account.service.js
@@ -0,0 +1,28 @@
+﻿(function() {
+    angular
+        .module("Wallet")
+        .factory("Account", account) 
+
+    account.$inject = ['$http'] 
+
+    function account() {
+        return {
+            register: register,
+            login: login,
+            logout: logout, 
+            users: listUsers, 
+        }
+        /////////////////
+        function register(email, passA, passB) {
+        }
+        function login(email, password) {
+        }
+        function logout() {
+        }
+        // Should belong to a more general Authentication service. 
+        //      Auth AuthStats SiteStats
+        function listUsers() {
+            return ["Alpha", "Beta", "Signor"]
+        }
+    }
+})()
