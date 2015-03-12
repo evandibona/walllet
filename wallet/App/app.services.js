0b61cd4ac3723c794be3b6c2e879660547cfb85c
Evan DiBona <evandibona@gmx.com>
2015-03-12 10:18:16 -0400

    Very, very basic template.

--- wallet/App/app.services.js
+++
@@ -1,3 +0,0 @@
-﻿angular
-    .module("Wallet", [])
-    .service() 
---
+++ wallet/App/main.register.ctlr.js
@@ -0,0 +1,17 @@
+﻿(function() {
+    angular
+        .module("Wallet")
+        .controller("Register", regCtrl) 
+
+    function regCtrl() {
+        var vm = this 
+        vm.user = ""
+        vm.pass = ""
+        vm.s = "-----"
+
+        vm.submit = function() {
+            vm.s = "!--M--!"
+        }
+        console.log(vm)
+    }
+})()
