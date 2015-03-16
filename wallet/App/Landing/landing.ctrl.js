e122b7885a6d067a3ac6213aeee431e1e5e922b2
Evan DiBona <evandibona@gmx.com>
2015-03-16 13:15:37 -0400

    Registration is functional. Le registrazione è funzionale.

--- wallet/App/Landing/landing.ctrl.js
+++ wallet/App/Landing/landing.ctrl.js
@@ -3,14 +3,34 @@
         .module("Wallet")
         .controller("Landing", landing) 
 
-    landing.$inject = ['Account'] 
+    landing.$inject = ['Account', '$http'] 
 
-    function landing(Account) {
+    function landing(Account, $http) {
         var vm = this
         vm.isRegister = false
         vm.flip = function () {
             if (vm.isRegister == true) { vm.isRegister = false  }
             if (vm.isRegister == false) { vm.isRegister = true }
+        }
+        vm.register = function () {
+            vm.regOut = {email: vm.email, password: vm.passA }
+            register(vm.email, vm.passA, vm.passB); 
+        }
+
+        /////////
+        function register(email, pA, pB) {
+            var password = ""
+            var response = {}
+            if (pA == pB) {
+                password = pA
+            }
+            $http.post("/api/Account/Register", { Email: email, Password: pA, ConfirmPassword: pB })
+            .success(function (data) {
+                vm.regResp = data
+            })
+            .error(function (data) {
+                vm.regResp = data
+            })
         }
     }
 })()
