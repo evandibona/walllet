5a85272e60ddc3972f6769b781c05b2013c79c8c
Evan DiBona <evandibona@gmx.com>
2015-03-16 09:34:36 -0400

    Was looking for home page solution.

---
+++ wallet/App/Services/app.service.js
@@ -0,0 +1,13 @@
+﻿(function() {
+    angular
+        .module("Wallet")
+        .factory("AppConfig", appConfig) 
+
+    function appConfig() {
+        return {
+            onLanding: false,
+        }
+        /////////////////
+        isOnLanding = false 
+    }
+})()
---
+++ wallet/App/app.ctrl.js
@@ -0,0 +1,12 @@
+﻿(function() {
+    angular
+        .module("Wallet")
+        .controller("WholeApp", app) 
+
+    app.$inject = ['AppConfig'] 
+
+    function app(AppConfig) {
+        var vm = this
+        vm.onLanding = AppConfig.onLanding
+    }
+})()
