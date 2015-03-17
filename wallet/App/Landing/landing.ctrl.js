77eee0e3b42d75ffb697e27677216b968f8aaa7b
Evan DiBona <evandibona@gmx.com>
2015-03-17 11:33:21 -0400

    Database builder.

--- wallet/App/Landing/landing.ctrl.js
+++ wallet/App/Landing/landing.ctrl.js
@@ -1,28 +1,28 @@
-﻿(function() {
+﻿(function () {
     angular
         .module("Wallet")
-        .controller("Landing", landing) 
+        .controller("Landing", landing)
 
-    landing.$inject = ['Account', '$http', '$location'] 
+    landing.$inject = ['Account', '$http', '$location']
 
     function landing(Account, $http, $location) {
         var vm = this
         vm.isRegister = false
 
         vm.flip = function () {
-            if (vm.isRegister == true) { vm.isRegister = false  }
+            if (vm.isRegister == true) { vm.isRegister = false }
             if (vm.isRegister == false) { vm.isRegister = true }
         }
-        vm.authenticate = function () {authenticate(vm.email, vm.passA, vm.passB)}
+        vm.authenticate = function () { authenticate(vm.email, vm.passA, vm.passB) }
 
         /////////
         function authenticate(email, A, B) {
             if (A != undefined && B != undefined) {
                 register(email, A, B)
             }
-            //login(email, A) 
-            $location.path("demodebug") 
+            login(email, A)
         }
+        // Register // 
         function register(email, pA, pB) {
             var password = ""
             var response = {}
@@ -31,7 +31,22 @@
             }
             $http.post("/api/Account/Register", { Email: email, Password: pA, ConfirmPassword: pB })
             .success(function (data) {
-                $location.path("/demodebug") 
+                $location.path("/demodebug")
+            })
+            .error(function (data) {
+                vm.error = data['ModelState']
+            })
+        }
+        // Login // 
+        function login(email, pA, pB) {
+            var password = ""
+            var response = {}
+            if (pA == pB) {
+                password = pA
+            }
+            $http.post("/api/Account/Register", { Email: email, Password: pA, ConfirmPassword: pB })
+            .success(function (data) {
+                $location.path("demodebug")
             })
             .error(function (data) {
                 vm.error = data['ModelState']
