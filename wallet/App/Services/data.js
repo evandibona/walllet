01b12e01d919c926fb39e6759c4e60c78a5f9e08
Evan DiBona <evandibona@gmx.com>
2015-03-25 10:25:56 -0400

    The dashboard template.

---
+++ wallet/App/Services/data.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    angular
+        .module("Wallet")
+        .factory("Dummy", dummyData)
+
+    function dummyData() {
+        // Lot's of bad practices in this :)
+
+        return {
+            // Households
+            // Relevant Types
+            //  pools, users, transactions, Streams(revenue, and expenses), BankAccounts, HistoricBankBalances, Actions (Historic)
+            households: [
+                ['Id', 'Name'], 
+                ['1', 'Smith'], 
+                ['2', 'Finkel'], 
+                ['3', 'Harold'], 
+            ],
+            pools: [
+                ['id', 'name', 'size', 'j']
+            ]
+        }
+    }
+})()
