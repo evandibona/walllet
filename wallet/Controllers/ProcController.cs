41dc0d3fb0c69cec08513d269ef9914c674bd0e6
Evan DiBona <evandibona@gmx.com>
2015-03-27 14:53:03 -0400

    Added mock procedure into the C#.

---
+++ wallet/Controllers/ProcController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.SqlClient;
+using Insight.Database;
+using System.Configuration;
+
+namespace Wallet.Controllers
+{
+    [Authorize]
+    public class ProcController : ApiController
+    {
+        // POST api/proc
+        public IEnumerable<string> Post(List<string> procedure)
+        {
+            var name = procedure[0];
+            procedure.RemoveAt(0);
+            var ps = procedure;
+
+            switch (name)
+            {
+                case "actionsOfHH":
+                    Procs.actionsOfHH(ps[0]);
+                    break;
+                default:
+                    break;
+            }
+            return new string[] { "value1", "value2" };
+        }
+    }
+
+    public class Procs
+    {
+        private static string connString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        private static SqlConnection conn = new SqlConnection(connString);
+        public static List<Dictionary<string, string>> actionsOfHH(string hh)
+        {
+            List<Dictionary<string, string>> values = conn.Execute("ActionsOfHH", new { HouseholdId = 22 });
+            return values;
+        }
+    }
+}
