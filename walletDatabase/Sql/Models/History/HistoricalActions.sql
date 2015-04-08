bf14d513f5dfff54ab1a4d4004a83f2570eb66ed
Evan DiBona <evandibona@gmx.com>
2015-04-08 14:15:08 -0400

    Dropped entire database and now everything is all good.

--- walletDatabase/Sql/Models/History/HistoricalActions.sql
+++ walletDatabase/Sql/Models/History/HistoricalActions.sql
@@ -1,20 +1,15 @@
 ﻿ CREATE TABLE [Models].[HistoricalActions]
  (
-    [Id]			int		IDENTITY(1,1) NOT NULL, --Identity( seed, increment ) 
-    [Action]		nvarchar(150)  	NOT NULL, 
-	[When]			date			NOT NULL, 
-	[UserId]		int				NOT NULL, 
-	[HouseholdId]	int				NOT NULL, 
+    [Id]			int		IDENTITY(1,1)	NOT NULL, --Identity( seed, increment ) 
+    [Action]		nvarchar(150)  			NOT NULL, 
+	[When]			date					NOT NULL, 
+	[UserId]		int						NOT NULL, 
+	[Household]		uniqueidentifier		NOT NULL, 
  )
  GO
 
 ALTER TABLE [Models].[HistoricalActions]
-ADD CONSTRAINT [PK_Models.HistoricalActions] PRIMARY KEY CLUSTERED
-(
-	[Id] ASC, 
-	[UserId] ASC, 
-	[HouseholdId] ASC 
-)
+	ADD CONSTRAINT [PK_Models.HistoricalActions] PRIMARY KEY CLUSTERED ( [Id] ASC )
 GO
 
 AUTOPROC INSERT [Models].[HistoricalActions] 
