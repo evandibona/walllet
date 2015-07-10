ef42023bd5cc3a22e93f58afcc0cc5010c1f5367
evandibona <evandibona@gmail.com>
2015-07-10 10:30:06 -0400

    Procedures to add and remove users from a household.

---
+++ walletDatabase/Sql/Models/Procedures/HhAddUser.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [Models].[HhAddUser] (
+	@HhId nvarchar(30), 
+	@UserId int 
+)
+AS
+
+-- Set the user's Household property to the House. --
+
+DECLARE @T TABLE( [Id] int )
+
+UPDATE [Security].[Users] SET
+	[HouseholdId] = @HhId
+OUTPUT 
+	Inserted.[Id] 
+INTO @T
+WHERE 
+	[Id]=@UserId
+SELECT * FROM @T
---
+++ walletDatabase/Sql/Models/Procedures/HhRevokeUser.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [Models].[HhRevokeUser] (
+	@UserId int 
+)
+AS
+
+DECLARE @T TABLE( [Id] int )
+
+UPDATE [Security].[Users] SET
+	[HouseholdId] = NULL
+OUTPUT 
+	Inserted.[Id] 
+INTO @T
+WHERE 
+	[Id]=@UserId
+SELECT * FROM @T
